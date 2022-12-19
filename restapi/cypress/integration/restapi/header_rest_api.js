c1e7345643b2c952b296c20d5572e57ac2e31506
hadidfathan <sieghartmchdol@gmail.com>
2022-12-19 12:02:12 +0700

    file sucsess

---
+++ restapi/cypress/integration/restapi/header_rest_api.js
@@ -0,0 +1,23 @@
+describe('Header Test Valid', () => {
+    it('Validate Header', () => {
+        cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon')
+        cy.get('@pokemon').its('headers').its('content-type').should('include' , 'application/json; charset=utf-8')
+        cy.get('@pokemon').its('status').should('equal', 200)
+        cy.get('@pokemon').should((response) => {
+            expect(response).to.have.property('headers')
+            expect(response.body).has.property('name')
+            
+        })
+        cy.get('@pokemon').should((response => {
+            expect(response.body).to.have.property('name', 'ditto')
+            expect(response.body).to.have.property('base_experience', 101)
+        }))
+    });
+    it('Validate body pokemon', () => {
+        cy.request('https://pokeapi.co/api/v2/version/7/').as('pokemon')
+        cy.get('@pokemon').its('headers').its('connection').should('include' , 'keep-alive')
+        cy.get('@pokemon').its('body').should('include', {name:'ruby'})
+        
+        
+    });
+})
